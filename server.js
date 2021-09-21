d8d8a52ca0522113565de3ff775f346e9c8b45a0
herbert <herberthtk100@gmail.com>
2021-09-21 08:52:11 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+const serverless = require('serverless-http');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}!`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    socket.join(user.room);
+
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+    // if (!user.room) {
+      
+    // }
+    console.log("message sent");
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left!`,
+      });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
+});
+
+// server.listen(PORT, () => {
+//   console.log(`Server has started on port: ${PORT}`);
+// });
+
+module.exports.handler = serverless(server);
